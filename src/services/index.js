01055bf225be46387e40fcbaccadcb453548b02e
Caio <bladellano@gmail.com>
2023-10-23 16:03:52 +0100

    :hammer:Corrigindo a ausencia do token na utilizacao de todos os metodos no front

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 const url = process.env.VUE_APP_BASE_URL;
+const localStorage = window.localStorage;
 
 const api = axios.create({
-    baseURL: url + "/api"
+    baseURL: url + "/api",
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+    }
 });
 
 export default api;
